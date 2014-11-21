87650a888827d6d09ac2251446184011f6693784
Tom <thomas.zumsteg@gmail.com>
2014-11-20 20:12:29 -0800

    First wordy itteration

---
+++ wordy/wordy.py
@@ -0,0 +1,25 @@
+"""Does a natrual language calculation"""
+from operator import add, sub, mul, div
+from re import finditer, match
+
+def calculate(string):
+    """Parses and preforms natrual language calculation"""
+    operators = {"plus": add,
+                 "minus": sub,
+                 "multiplied by": mul,
+                 "divided by": div,
+                }
+    start = "What is"
+    function = " (" + "|".join(operators.keys()) + ")"
+    number = r" (-?\d+)"
+    regex = start + "(?P<first>" + number + ")" + "(?P<calculation>" +\
+            "(" + function + number + ")+)" + r"\?$"
+    regex_match = match(regex, string)
+    if not regex_match:
+        raise ValueError
+    val = int(regex_match.group('first'))
+    remaining = regex_match.group('calculation')
+    for step in finditer(function + number, remaining):
+        (func, num) = step.groups()
+        val = operators[func](val, int(num))
+    return val
