70b752c213788febef973a55a41b980ddd8016bd
Tom <thomas.zumsteg@gmail.com>
2014-11-22 16:51:04 -0800

    start on zebra_puzzle

---
+++ zebra-puzzle/zebra_puzzle.py
@@ -0,0 +1,9 @@
+
+def solution():
+    properties = {"color": ["red", "green", "ivory", "yellow", "blue"],
+                  "animal": ["dog", "snails", "fox", "horse", "zebra"],
+                  "drink": ["orange juice", "mink", "coffee", "tea", "water"],
+                  "race": ["Englishman", "Spaniard", "Ukrainian", "Norwegian", "Jamanese"],
+                  "smokes": ["Old Gold", "Kools", "Lucky Strike", "Chesterfields", "Parliaments"],
+                  "number": range(5)
+                 }
