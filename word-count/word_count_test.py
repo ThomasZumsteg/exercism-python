72720da9d3f9d84e6d2792f05313ecb95f5ffa25
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-02-22 09:37:30 -0800

    Updated word-count to match new test case

--- word-count/word_count_test.py
+++ word-count/word_count_test.py
@@ -1,9 +1,19 @@
+# -*- coding: utf-8 -*-
 import unittest
 
 from wordcount import word_count
 
 
+# to be backwards compatible with the old Python 2.X
+def decode_if_needed(string):
+    try:
+        return string.decode('utf-8')
+    except AttributeError:
+        return string
+
+
 class WordCountTests(unittest.TestCase):
+
     def test_count_one_word(self):
         self.assertEqual(
             {'word': 1},
@@ -24,7 +34,7 @@ class WordCountTests(unittest.TestCase):
 
     def test_preserves_punctuation(self):
         self.assertEqual(
-            {'car': 1, 'carpet': 1, 'as': 1, 'java': 1, ':': 2, 'javascript!!&@$%^&': 1},
+            {'car': 1, 'carpet': 1, 'as': 1, 'java': 1, 'javascript': 1},
             word_count('car : carpet as java : javascript!!&@$%^&')
         )
 
@@ -36,21 +46,42 @@ class WordCountTests(unittest.TestCase):
 
     def test_mixed_case(self):
         self.assertEqual(
-            {'go': 1, 'Go': 1, 'GO': 1},
-            word_count('go Go GO')
+            [2, 3],
+            sorted(list(word_count('go Go GO Stop stop').values()))
         )
-        
+
     def test_multiple_spaces(self):
-      self.assertEqual(
-          {'wait': 1, 'for': 1, 'it': 1},
-          word_count('wait for       it')
-      )
-        
+        self.assertEqual(
+            {'wait': 1, 'for': 1, 'it': 1},
+            word_count('wait for       it')
+        )
+
     def test_newlines(self):
         self.assertEqual(
-            {'rah': 2, 'ah': 3, 'roma': 2, 'ma': 1, 'ga': 2, 'oh': 1, 'la': 2, 
-            'want': 1, 'your': 1, 'bad': 1, 'romance': 1},
-            word_count('rah rah ah ah ah\nroma roma ma\nga ga oh la la\nwant your bad romance')
+            {'rah': 2, 'ah': 3, 'roma': 2, 'ma': 1, 'ga': 2, 'oh': 1, 'la': 2,
+             'want': 1, 'your': 1, 'bad': 1, 'romance': 1},
+            word_count('rah rah ah ah ah\nroma roma ma\n'
+                       'ga ga oh la la\nwant your bad romance')
+        )
+
+    def test_tabs(self):
+        self.assertEqual(
+            {'rah': 2, 'ah': 3, 'roma': 2, 'ma': 1, 'ga': 2, 'oh': 1, 'la': 2,
+             'want': 1, 'your': 1, 'bad': 1, 'romance': 1},
+            word_count('rah rah ah ah ah\troma roma ma\tga ga oh la la\t'
+                       'want your bad romance')
+        )
+
+    def test_non_alphanumeric(self):
+        self.assertEqual(
+            {'hey': 1, 'my': 1, 'spacebar': 1, 'is': 1, 'broken': 1},
+            word_count('hey,my_spacebar_is_broken.')
+        )
+
+    def test_unicode(self):
+        self.assertEqual(
+            {decode_if_needed('до'): 1, decode_if_needed('свидания'): 1},
+            word_count('до🖖свидания!')
         )
 
 if __name__ == '__main__':
--- word-count/wordcount.py
+++ word-count/wordcount.py
@@ -1,6 +1,11 @@
 """Catalogs and counts the number of words used in a phrase"""
 from collections import Counter
+import re
 
 def word_count(phrase):
     """Returns dictionary with a count of words used"""
-    return Counter(phrase.split())
+    return Counter(word for word in 
+        re.split(r'[\W_]+',
+                 phrase.lower().decode('utf-8'),
+                 flags=re.UNICODE)
+        if word)
