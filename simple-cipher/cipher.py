d2b62228438dbc76de296447e3b4381459617740
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-03-03 09:44:39 -0800

    Updated simple_cipher
    Fixed a bug in the _shift method
    Added checking for the key

--- simple-cipher/cipher.py
+++ simple-cipher/cipher.py
@@ -7,10 +7,11 @@ class Cipher(object):
 
     def __init__(self, key=None):
         """Use a custom key or a random string of 100 lower case characters"""
-        if key:
-            self.key = key
-        else:
-            self.key = ''.join([choice(ascii_lowercase) for _ in range(100)])
+        if not key:
+            key = ''.join([choice(ascii_lowercase) for _ in range(100)])
+        elif not key.isalpha() or not key.islower():
+            raise ValueError("Key needs to be lower case letters only: %s" % key)
+        self.key = key
 
     def encode(self, clear_text):
         """Encodes text using key"""
@@ -26,7 +27,7 @@ class Cipher(object):
     def _shift(text, key_func):
         """Shifts characers in text according to a key function"""
         text = [c for c in text.lower() if c.isalpha()]
-        shift_dist = lambda x, s: chr((ord(c)-97+s) % 26 + 97)
+        shift_dist = lambda x, s: chr((ord(x)-97+s) % 26 + 97)
         return ''.join([shift_dist(c, k) for c, k in zip(text, key_func)])
 
     @staticmethod
