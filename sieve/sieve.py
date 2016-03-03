b959a457a2ccb0b74f1bf8c10c601b929d18e3ae
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-03-03 09:51:57 -0800

    Fixed bug in sieve
    range is now inclusive on the high end

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -4,7 +4,7 @@ def sieve(lim):
     """Implimentation of sieve of Eratosthenes"""
     multiples = set()
     primes = [2,]
-    for num in range(3, lim, 2):
+    for num in range(3, lim+1, 2):
         if num not in multiples:
             primes.append(num)
             multiples.update(range(num**2, lim, num))
