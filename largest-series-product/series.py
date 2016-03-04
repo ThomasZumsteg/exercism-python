9a2a4710779f5b0a643b4da9bb898c740ba506b8
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-03-04 10:58:22 -0800

    Updated largest-series-product to remove reduce for python 3

--- largest-series-product/series.py
+++ largest-series-product/series.py
@@ -3,17 +3,21 @@
 def largest_product(digits, size):
     """Finds the largest product of slices of a given size"""
     # Why does a blank set of digits have a maximum product of 1?
-    if digits == '':
-        return 1
     slice_list = slices(digits, size)
-    mult_reduce = lambda l: reduce(lambda x, y: x * y, l)
+    def mult_reduce(items):
+        total = 1
+        for i in items:
+            total *= i
+        return total
     slice_list = [mult_reduce(l) for l in slice_list]
     return max(slice_list)
 
 def slices(digits, size):
     """Returns list of lists of consecutive digits"""
-    if size <= 0 or size > len(digits):
+    if not 0 <= size <= len(digits):
         raise ValueError
+    elif digits == '':
+        return [[1]]
 
     slice_list = []
 
