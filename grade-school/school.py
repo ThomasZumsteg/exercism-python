94a59488f0bdc833c7e6d4f97070e9a16709ad3e
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-03-05 11:36:05 -0800

    Updated grade-school to match new tests
    sort returns sequence instead of dictionary

--- grade-school/school.py
+++ grade-school/school.py
@@ -11,12 +11,12 @@ class School(object):
     def add(self, student, level):
         """Adds students to the db"""
         self.db[level].add(student)
-        
+
     def grade(self, level):
         """Returns the students in a grade"""
         return self.db[level]
 
     def sort(self):
         """Returns the whole school in alphabetical order"""
-        return {grade: tuple(sorted(students)) 
-                for grade, students in self.db.iteritems()}
+        return ((grade, tuple(sorted(students)))
+                for grade, students in self.db.items())
