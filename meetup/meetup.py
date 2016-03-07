4320ca8a44120975c3389f41d751c471b53909c4
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-03-07 09:35:14 -0800

    Updated meetup match new tests
    Added 5th as option
    Added AssertionError when the day doesn't exist
    Moved some functionality into the global context

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -8,45 +8,47 @@ def meetup_day(year, month, weekday, condition):
     """Returns date object for a given year, month, weekday and given condition
     Current conditions [1-4]th occurence, last, teenth
     """
-
-    # Conversion of string weekday to number
-    weekday_to_num = {'Monday'   : 0,
-                      'Tuesday'  : 1,
-                      'Wednesday': 2,
-                      'Thursday' : 3,
-                      'Friday'   : 4,
-                      'Saturday' : 5,
-                      'Sunday'   : 6,
-                     }
-
-    def last(day):
-        """True on the last week of the month"""
-        return day.month != (day + datetime.timedelta(days=7)).month
-
-    def nth(n):
-        """Returns a function that is true on the nth week of a month"""
-        def nth_worker(day):
-            """True on the nth week of the month"""
-            return (day.day - 1) // 7 == n-1
-        return nth_worker
-
-    def teenth(day):
-        """True on a days ending in -teenth, ie seventeenth"""
-        return 13 <= day.day and day.day <= 19
-
-    # Maps condition to function test
-    conditions = {'teenth': teenth,
-                  'last': last,
-                  '1st': nth(1),
-                  '2nd': nth(2),
-                  '3rd': nth(3),
-                  '4th': nth(4),
-                 }
-
     day = datetime.date(year, month, 1)
     # Tests everyday in the month until the weekday and condition are met
     while day.month == month:
-        if day.weekday() == weekday_to_num[weekday] and \
-            conditions[condition](day):
+        if day.weekday() == WEEKDAY_TO_NUM[weekday] and \
+            CONDITIONS[condition](day):
             return day
         day += datetime.timedelta(days=1)
+    raise AssertionError("Not a valid meetup day")
+
+# Conversion of string weekday to number
+WEEKDAY_TO_NUM = {'Monday'   : 0,
+                  'Tuesday'  : 1,
+                  'Wednesday': 2,
+                  'Thursday' : 3,
+                  'Friday'   : 4,
+                  'Saturday' : 5,
+                  'Sunday'   : 6,
+                 }
+
+def last(day):
+    """True on the last week of the month"""
+    return day.month != (day + datetime.timedelta(days=7)).month
+
+def nth(n):
+    """Returns a function that is true on the nth week of a month"""
+    def nth_worker(day):
+        """True on the nth week of the month"""
+        return (day.day - 1) // 7 == n-1
+    return nth_worker
+
+def teenth(day):
+    """True on a days ending in -teenth, ie seventeenth"""
+    return 13 <= day.day and day.day <= 19
+
+# Maps condition to function test
+CONDITIONS = {'teenth': teenth,
+              'last': last,
+              '1st': nth(1),
+              '2nd': nth(2),
+              '3rd': nth(3),
+              '4th': nth(4),
+              '5th': nth(5),
+             }
+
