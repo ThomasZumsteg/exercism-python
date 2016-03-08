30d9fd64a3e122572ebad65365f3fcc59c539002
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-03-08 09:38:59 -0800

    Update orc to pass new tests
    Handles more than 0 and 1
    Reads and writes multiple digits

--- ocr-numbers/ocr.py
+++ ocr-numbers/ocr.py
@@ -10,19 +10,71 @@ FONT = {'0': [" _ ",
               "  |",
               "   "
              ],
+        '2': [" _ ",
+              " _|",
+              "|_ ",
+              "   "
+             ],
+        '3': [" _ ",
+              " _|",
+              " _|",
+              "   "
+             ],
+        '4': ["   ",
+              "|_|",
+              "  |",
+              "   "
+             ],
+        '5': [" _ ",
+              "|_ ",
+              " _|",
+              "   "
+             ],
+        '6': [" _ ",
+              "|_ ",
+              "|_|",
+              "   "
+             ],
+        '7': [" _ ",
+              "  |",
+              "  |",
+              "   "
+             ],
+        '8': [" _ ",
+              "|_|",
+              "|_|",
+              "   "
+             ],
+        '9': [" _ ",
+              "|_|",
+              " _|",
+              "   "
+             ],
        }
 
-INVERSE_FONT = dict((tuple(v), k) for k, v in FONT.iteritems())
+INVERSE_FONT = dict((tuple(v), k) for k, v in FONT.items())
 
-def number(num):
-    """Convert a string of characters into a single digiti"""
-    if len(num) != 4 or not all(len(row) == 3 for row in num):
+def number(rows):
+    """number converts pipes and bars representation of numbers to digits"""
+    row_len = len(rows[0])
+    if len(rows) != 4 or any(len(row) != row_len for row in rows):
         raise ValueError
-    try:
-        return INVERSE_FONT[tuple(num)]
-    except KeyError:
-        return "?"
+    digits = ""
+    for n in range(0, row_len, 3):
+        digit = [rows[i][n:n+3] for i in range(4)]
+        try:
+            digits += INVERSE_FONT[tuple(digit)]
+        except KeyError:
+            digits += "?"
+    return digits
 
-def grid(num):
-    """Convert a digit into a ocr digit"""
-    return FONT[num]
+def grid(digits):
+    """grid converts digits to a pipes and bars representation"""
+    grid = ["","","",""]
+    for digit in digits:
+        try:
+            for i, row in enumerate(FONT[digit]):
+                grid[i] += row
+        except KeyError:
+            raise ValueError
+    return grid
