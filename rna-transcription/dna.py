ad5f7669153ac5a958b55ef40804d3eef9cb97a1
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-03-09 09:45:26 -0800

    Updated rna-transcription for python 3
    Changed string.maketrans to str.maketrans

--- rna-transcription/dna.py
+++ rna-transcription/dna.py
@@ -1,7 +1,5 @@
 """Turns DNA sequence into RNA sequence"""
 
-from string import maketrans
-
 def to_rna(dna):
     """Makes RNA sequence from DNA sequence"""
-    return dna.translate(maketrans('GCTA','CGAU'))
+    return dna.translate(str.maketrans('GCTA','CGAU'))
