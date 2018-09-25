f74c14f6433a6ab2da0573096a54675972c79286
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-09-25 16:28:19 +0000

    First iteration ledger

--- ledger/ledger.py
+++ ledger/ledger.py
@@ -20,83 +20,38 @@ def create_entry(date, description, change):
     entry.change = change
     return entry
 
-
-def us_date(day, month, year):
-    return '{:02d}/{:02d}/{:04d}'.format(month, day, year)
-
-def nl_date(day, month, year):
-    return '{:02d}-{:02d}-{:04d}'.format(day, month, year)
-
-def us_number(amount, currency):
-    fmt_str = '{}{:s}.{:02d} '
-    if amount < 0:
-        fmt_str = '({}{:s}.{:02d})'
-    units, change = abs(amount) // 100, abs(amount) % 100
-    digits = str(units)[::-1]
-    return fmt_str.format(
-        currency,
-        ','.join(reversed(list(''.join(
-            reversed(digits[l:l+3])) for l in range(0, len(digits), 3)))),
-        change)
-
-def nl_number(amount, currency):
-    fmt_str = '{} {:s},{:02d} '
-    if amount < 0:
-        fmt_str = '{} -{:s},{:02d} '
-    units, change = abs(amount) // 100, abs(amount) % 100
-    digits = str(units)[::-1]
-    return fmt_str.format(
-        currency,
-        '.'.join(reversed(list(''.join(
-            reversed(digits[l:l+3])) for l in range(0, len(digits), 3)))),
-        change)
+def number(pos, neg, sep):
+    def wrapped(amount, currency):
+        fmt_str = pos if amount >= 0 else neg 
+        units, change = abs(amount) // 100, abs(amount) % 100
+        digits = str(units)[::-1]
+        return fmt_str.format(
+            currency,
+            sep.join(reversed(list(''.join(
+                reversed(digits[l:l+3])) for l in range(0, len(digits), 3)))),
+            change)
+    return wrapped
 
 def format_entries(currency, locale, entries):
     localization = {
         'en_US':  Local(
              {"Date": "Date", "Description": "Description", "Change": "Change"},
-            us_date,
-            us_number),
+            lambda day, month, year: '{:02d}/{:02d}/{:04d}'.format(month, day, year),
+            number('{}{:s}.{:02d} ', '({}{:s}.{:02d})', ',')),
         'nl_NL': Local(
             {"Date": "Datum", "Description": "Omschrijving", "Change": "Verandering"},
-            nl_date,
-            nl_number)
+            lambda day, month, year: '{:02d}-{:02d}-{:04d}'.format(day, month, year),
+            number('{} {:s},{:02d} ', '{} -{:s},{:02d} ', '.'))
     }
+    symbols = {"USD": "$", "EUR": "€"}
     # Generate Header Row
     config = localization[locale]
     table = ["{Date:10s} | {Description:25s} | {Change:13s}".format(**config.dict)]
-    while len(entries) > 0:
-        table.append('')
-
-        # Find next entry in order
-        min_entry_index = -1
-        for i, entry in enumerate(entries):
-            if min_entry_index < 0:
-                min_entry_index = i
-                continue
-            min_entry = entries[min_entry_index]
-            if entry.date < min_entry.date:
-                min_entry_index = i
-                continue
-            if (
-                entry.date == min_entry.date and
-                entry.change < min_entry.change
-            ):
-                min_entry_index = i
-                continue
-            if (
-                entry.date == min_entry.date and
-                entry.change == min_entry.change and
-                entry.description < min_entry.description
-            ):
-                min_entry_index = i
-                continue
-        entry = entries[min_entry_index]
-        entries.pop(min_entry_index)
+    for entry in sorted(entries, key=lambda e: (e.date, e.change, e.description)):
 
         desc = entry.description
-        table[-1] += '{:s} | {:<25s} | {:>13s}'.format(
+        table.append('{:s} | {:<25s} | {:>13s}'.format(
                 config.date(entry.date.day, entry.date.month, entry.date.year),
                 desc if len(desc) <= 25 else desc[:22] + '...',
-                config.number(entry.change, '$' if currency == "USD" else u'€'))
+                config.number(entry.change, symbols[currency])))
     return '\n'.join(table)
